f7bb03bf55437c35da567e2cbd444c9dc5591533
Adam Sung Min Park <parksm2@uci.edu>
2022-10-26 00:20:08 +0900

    added cors

--- dream/src/main/java/com/example/dream/DreamApplication.java
+++ dream/src/main/java/com/example/dream/DreamApplication.java
@@ -3,11 +3,31 @@ package com.example.dream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class DreamApplication {
     public static void main(String[] args) {
         SpringApplication.run(DreamApplication.class, args);
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        configuration.addExposedHeader("*");
+        configuration.addExposedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
+
     }
 }
